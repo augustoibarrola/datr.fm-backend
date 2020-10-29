e92c34a5601b0ea4dc24199aa4acf1d5a89e1fb6
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-29 11:40:05 -0400

    user#index and user#show created

--- app/models/heart.rb
+++ app/models/heart.rb
@@ -2,6 +2,3 @@ class Heart < ApplicationRecord
     belongs_to :liker, class_name: 'User'
     belongs_to :liked, class_name: 'User'
 end
-
-# liker and liked refer to the User class both, and both also have
-# counterparts in the Hearts table, liker_id and liked_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-    # the "liker" User has_many "liked" users 
+
     has_many :liked_users, foreign_key: :liker_id, class_name: 'Heart'
     has_many :likeds, through: :liked_users
 
-    # the "liked" User has_many "liker" users 
     has_many :liker_users, foreign_key: :liked_id, class_name: 'Heart'
     has_many :likers, through: :liker_users
 
