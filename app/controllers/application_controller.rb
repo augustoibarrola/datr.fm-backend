d1dfa435f768e5406f810cda3700edbd4744adc5
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-30 17:56:02 -0400

    created serializers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,41 @@
 class ApplicationController < ActionController::API
+
+    # before_action :authorized
+    # uncommenting the above will make it so that access to the API is not granted
+    # unless user has successfully logged into db
+    
+    def encode_token(payload)
+        JWT.encode(payload, 's3cret')
+
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 's3cret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user 
+        if decoded_token 
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in? 
+        !!current_user
+    end
+
+    def authorized 
+        render json: { message: 'Please Log In' }, status: :unauthorized unless logged_in?
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorized, only: [:create]
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
     def index 
         users = User.all
@@ -18,7 +23,8 @@ class UsersController < ApplicationController
         @user = User.create(user_params)
 
         if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created 
+            @token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created 
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
