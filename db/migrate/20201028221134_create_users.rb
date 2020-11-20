f4e757df0e52f31a1007e0ddfecb867a8d274f1f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-11-20 11:52:28 -0500

    yes

--- db/migrate/20201028221134_create_users.rb
+++ db/migrate/20201028221134_create_users.rb
@@ -2,7 +2,10 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.integer :age
       t.string :username
+      t.string :lastfm_username
+      t.string :spotify_username
       t.string :email
       t.text :description
       t.string :password_digest
